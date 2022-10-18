577ee53889a983eaa03eb14a9d421e069d2ff2c6
hyae4924 <hyae4924@gmail.com>
2022-10-19 01:19:01 +0900

    learn file module

---
+++ 9_file/read_write_create/app.js
@@ -0,0 +1,30 @@
+const fs = require("fs").promises;
+
+// read a file
+fs.readFile("./text.txt") //buffer 형태로
+  .then(console.log)
+  .catch(console.error);
+fs.readFile("./text.txt", "utf8") //인코딩된 형태로
+  .then(console.log)
+  .catch(console.error);
+
+//write a file
+fs.writeFile("./file.txt", "I'm always overiding") //(파일이 없으면 만들고,있으면 덮어씀)
+  .catch(console.log);
+
+fs.appendFile("./file.txt", " but appedFile not overriding just add ") //
+  .then(() => {
+    fs.copyFile("./file.txt", "./file2.txt") //
+      .catch(console.error);
+  })
+  .catch(console.error);
+// 위 코드(12,15)는 어떤게 먼저 실행될지 모름
+
+// make folder
+fs.mkdir("sub-folder") //
+  .catch(console.log);
+
+// 디렉토리안모든 파일읽기(배열안에 스트링으로리턴됨)
+fs.readdir("./") //
+  .then(console.log)
+  .catch(console.error);
---
+++ 9_file/rename/app.js
@@ -0,0 +1,28 @@
+// 시간이 많이드는 작업인 api는 3가지 형태로 제공된다
+// 1. rename(...,callback(error,data))  : 비동기
+// 2. try{renameSync(...)} catch(error){}   : 동기
+// 3. promises.rename().then().cathch()  : promise 형태
+
+const fs = require("fs");
+
+// rename
+try {
+  fs.renameSync("./text.txt", "./text-new.txt");
+} catch (error) {
+  console.log(error);
+}
+
+fs.rename("./text-new.txt", "./text.txt", error => {
+  console.log(error);
+});
+
+fs.promises
+  .rename("./text2.txt", "./text2-new.txt") //
+  .then()
+  .catch(console.log);
+
+//  두개의 비동기들중
+// 콜백함수는 태스크 큐에
+// 프로미스 함수는 마이크로 태스크 큐에
+// 즉 각각 다른 큐에 들어 가게 된다
+// 해서 어떤게 먼저 실행될지 알수 없다
